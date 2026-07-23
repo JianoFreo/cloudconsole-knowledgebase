7a2fe4d0e0a94889f8d751109f5b43a78e892062
Jiano Freo <jianofreo.magtangob@tup.edu.ph>
2026-07-23 12:43:31 +0800

    rmeoved the the web folder

--- web/src/pages/DepartmentPage.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate, useParams } from "react-router"
-
-function DepartmentPage() {
-    const { slug } = useParams<{ slug: string }>()
-    return <Navigate to={`/browse?department=${slug}`} replace />
-}
-
-export default DepartmentPage
